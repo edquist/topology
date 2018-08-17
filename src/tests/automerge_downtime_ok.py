f5a5e71c96369d01b57bdd6487c0b2c7026d58b0
Carl Edquist <edquist@cs.wisc.edu>
2018-08-17 14:33:23 -0500

    diff modified downtime files per downtime (SOFTWARE-3325)

--- src/tests/automerge_downtime_ok.py
+++ src/tests/automerge_downtime_ok.py
@@ -23,6 +23,15 @@ def main(args):
         else:
             errors += ["File '%s' is not a downtime file." % fname]
 
+    for fname in DTs:
+        dtdict_base = get_downtime_dict_at_version(BASE_SHA, fname)
+        dtdict_new  = get_downtime_dict_at_version(MERGE_COMMIT_SHA, fname)
+        dtminus, dtplus = diff_dtdict(dtdict_base, dtdict_new)
+        for dt in dtminus:
+            print("Old Downtime %d modified: %s" % (dt["ID"], dt))
+        for dt in dtminus:
+            print("New Downtime %d modified: %s" % (dt["ID"], dt))
+
     print_errors(errors)
     sys.exit(len(errors) > 0)
 
@@ -43,9 +52,9 @@ def get_modified_files(sha_a, sha_b):
         sys.exit(1)
     return ret.rstrip('\0').split('\0')
 
-def runcmd(cmdline):
+def runcmd(cmdline, stderr=None):
     from subprocess import Popen, PIPE
-    p = Popen(cmdline, stdout=PIPE)
+    p = Popen(cmdline, stdout=PIPE, stderr=stderr)
     out, err = p.communicate()
     return p.returncode, out
 
@@ -56,6 +65,27 @@ def print_errors(errors):
             print(e)
     else:
         print("Commit is eligible for auto-merge.")
+
+def get_file_at_version(sha, fname):
+    args = ['git', 'show', '%s:%s' % (sha, fname)]
+    ret, out = runcmd(args, open("/dev/null", "w"))
+    return out
+
+def get_downtime_dict_at_version(sha, fname):
+    txt = get_file_at_version(sha, fname)
+    dtlist = yaml.safe_load(txt) if txt else []
+    return dict( (dt["ID"], dt) for dt in dtlist )
+
+def diff_dtdict(dtdict_a, dtdict_b):
+    def dt_changed(ID):
+        return dtdict_a[ID] != dtdict_b[ID]
+    dtids_a = set(dtdict_a)
+    dtids_b = set(dtdict_b)
+    dtids_mod = filter(dt_changed, dtids_a & dtids_b)
+    dt_a = [ dtdict_a[ID] for ID in (dtids_a - dtids_b) | dtids_mod ]
+    dt_b = [ dtdict_b[ID] for ID in (dtids_b - dtids_a) | dtids_mod ]
+
+    return dt_a, dt_b
 
 if __name__ == '__main__':
     main(sys.argv[1:])
